0f85b4a33bcc240d74fb40b1c94f4610580027e5
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-07-25 23:46:31 -0300

    Test custom messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { ErrorMessage } from "./ErrorMessage"
 import { Form } from "./Form"
+import { ErrorMessageDeclaration, messages } from "./module"
+
+const customMessages : ErrorMessageDeclaration[] = [
+  {
+    name: "Name",
+    messages: {
+      required: "Hola capo. No tenes nombre?"
+    }
+  }
+]
+
+const defaultMessages : messages = {
+  required: "Debes completar este campo"
+}
 
 function App() {
   return (
     <>
-      <Form action="">
-        <label htmlFor="name">Name</label>
+      <Form action="" customMessages={customMessages} defaultMessages={defaultMessages}>
+        <label htmlFor="Name">Name</label>
         <input type="text" name="Name" required={true}/>
         <ErrorMessage htmlFor="Name"/>
-        <input type="email" name="Email" required={true} maxLength={30}/>
+        <input type="email" name="Email" required={true} minLength={2} maxLength={30}/>
         <ErrorMessage htmlFor="Email"/>
         <input type="submit" value="Send"/>
       </Form>
